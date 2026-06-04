14fd4488c641d94fe7849d9f69a67423f8be1735
Prakash Tiwari <prakashh.m.tiwari@gmail.com>
2026-06-04 11:04:06 +0530

    Updated default backend port

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-const BACKENDPORT = process.env.BACKENDPORT || 3000;
+const BACKENDPORT = process.env.BACKENDPORT || 5000;
 const BACKENDHOST = process.env.BACKENDHOST || 'localhost';
 
 const PORT = process.env.PORT || 3000;
